059e449263515ab25b17854c335e7e2cf9932e73
stronk-exe <ael-asri@student.1337.ma>
2023-12-22 22:58:17 +0100

    editing auth controller

--- backend/database/dbConfig.ts
+++ backend/database/dbConfig.ts
@@ -2,11 +2,15 @@ const { Pool } = require('pg');
 require('dotenv').config()
 
 const pool = new Pool({
-    user: process.env.DB_USER,//'stronk',
-    host: process.env.DB_HOST,//'postgresdb',
-    database: process.env.DB_NAME,//'matchadb',
-    password: process.env.DB_PASSWORD,//'pass',
-    port: process.env.DB_PORT,//5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
+
+pool.on('connect', () => {
+  console.log('Connected to database..')
+})
 
 module.exports = pool;
